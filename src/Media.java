9c754a50396a867d6898a82be7970eb21ef570b4
anaving <contato.unf@gmail.com>
2024-11-22 10:47:27 -0300

    first commit

---
+++ src/Media.java
@@ -0,0 +1,12 @@
+public class Media {
+    public static void main(String args[]) {
+        int nota1 = 10;
+        int nota2 = 9;
+        int nota3 = 8;
+        int nota4 = 9;
+
+        int media = nota1 + nota2 + nota3 + nota4;
+        int resultado = media / 4;
+        System.out.println(resultado);
+    }
+}
