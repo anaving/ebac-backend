83de3cb0c2ab27bff806eac049facd1de165ff5b
anaving <contato.unf@gmail.com>
2024-11-25 10:26:24 -0300

    first commit

---
+++ src/Exemplo.java
@@ -0,0 +1,11 @@
+public class Exemplo {
+    public static void main(String args[]) {
+
+        int num = 1283013;
+        System.out.println(num);
+
+        Long num2 = (long) num;
+        System.out.println(num2);
+
+    }
+}
